0d654fef90f69b3a4ed5e0901eac608d7ed93dd7
apelsin16 <apelsin@i.ua>
2024-10-13 00:13:58 +0200

    ready

--- src/2.ts
+++ src/2.ts
@@ -33,7 +33,7 @@ class Employee {
 
 class Manager extends Employee {
   constructor(name: string, department: string, salary: number) {
-    super(name, department, salary + 10000);
+      super(name, department, salary + 10000);
   }
   // Реалізуйте конструктор та збільшіть salary на 10000
 }
